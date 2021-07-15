d0d4629a627a9f1618950c29bb4f572bb142693b
DMDay2 <damon.maldonado.1@gmail.com>
2021-07-15 14:41:09 -0700

    Prototyping

--- main.c
+++ main.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*When calling a function that returns something, define the function before it is called*/
-double cube(double num) { /*note the input validation (double num) & the function name (cube)*/
-    double result = num * num * num;
-    return result;
+/*https://www.youtube.com/watch?v=KJgsSFOSQv0&t*/
 
-/*Could also write:
-double cube(double num)
-    return num * num * num;;
-*/
+/*When calling a function that returns something, define the function before it is called
+This is called prototyping which allows you to define a function after it is called*/
+double cube(double num);
 
-}
-
-int main() /*main function is a special function that ALWAYS gets called*/
+int main()
 {
-    /*https://www.youtube.com/watch?v=KJgsSFOSQv0&t*/
     printf("===Return Statements===\n\n");
-    printf("Answer: %f\n\n", cube(3.0));
+    printf("Answer: %f\n\n", cube(3.0)); /*CALLED HERE*/
 
     return 0;
+}
+
+double cube(double num) { /*DEFINED HERE*/
+    return num * num * num;
 }
