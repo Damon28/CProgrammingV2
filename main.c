3680a10bfc4597efed8cf0aaf51e1ec9be485248
DMDay2 <damon.maldonado.1@gmail.com>
2021-07-15 12:54:30 -0700

    added inputs & whatnot

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main() /*main function is a special function that ALWAYS gets called*/
 {
     /*https://www.youtube.com/watch?v=KJgsSFOSQv0&t*/
-    sayHi();
+    sayHi("Mike", 29); /*calls the function below*/
+    /*the string above is passed to the name variable below*/
+    sayHi("Tom", 18);
+    sayHi("Oscar", 19);
 
     return 0;
 }
 
-void sayHi(){ /*'void' means that the function does NOT return any info*/
-    printf("Hello User");
+void sayHi(char name[], int age){ /*'void' means that the function does NOT return any info*/
+    /*the above shows that sayHi takes a string & stores it into the 'name' variable*/
+    printf("Hello %s, you are %d.\n\n", name, age);
 }
