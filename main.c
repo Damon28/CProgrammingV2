81d9c3a0e60abcb297ccbf083c6ea25dd37200a6
DMDay2 <damon.maldonado.1@gmail.com>
2021-07-15 14:37:03 -0700

    Wrote a function in conjuction with main to cube an int

--- main.c
+++ main.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*When calling a function that returns something, define the function before it is called*/
+double cube(double num) { /*note the input validation (double num) & the function name (cube)*/
+    double result = num * num * num;
+    return result;
+
+/*Could also write:
+double cube(double num)
+    return num * num * num;;
+*/
+
+}
+
 int main() /*main function is a special function that ALWAYS gets called*/
 {
     /*https://www.youtube.com/watch?v=KJgsSFOSQv0&t*/
-    sayHi("Mike", 29); /*calls the function below*/
-    /*the string above is passed to the name variable below*/
-    sayHi("Tom", 18);
-    sayHi("Oscar", 19);
+    printf("===Return Statements===\n\n");
+    printf("Answer: %f\n\n", cube(3.0));
 
     return 0;
-}
-
-void sayHi(char name[], int age){ /*'void' means that the function does NOT return any info*/
-    /*the above shows that sayHi takes a string & stores it into the 'name' variable*/
-    printf("Hello %s, you are %d.\n\n", name, age);
 }
