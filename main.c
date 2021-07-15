fc9f0459e5cf9d5dee1337bc12c628d0371d0574
DMDay2 <damon.maldonado.1@gmail.com>
2021-07-15 11:43:25 -0700

    Simple Calc

--- main.c
+++ main.c
@@ -3,6 +3,27 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("===Simple Calculator w/ Integers===\n\n");
+    int num1;
+    int num2;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second Number: ");
+    scanf("%d", &num2);
+
+    printf("Answer : %d", num1 + num2);
+
+    printf("\n\n===Simple Calculator w/ Doubles===\n\n");
+
+    double num3;
+    double num4;
+    printf("Enter first number: ");
+    scanf(" %lf", &num3);
+    printf("Enter second Number: ");
+    scanf(" %lf", &num4);
+    printf("Answer : %f", num3 + num4);
+
+    printf("\n\nNOTE: For 'printf' use '%%f' to represent a double but...\nFor 'scanf' use '%%lf' to represent a double.\n\n");
+
     return 0;
 }
