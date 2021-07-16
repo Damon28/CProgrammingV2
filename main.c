bac986f0988f482d2804f262e8f7c05869ccc2d7
DMDay2 <damon.maldonado.1@gmail.com>
2021-07-16 04:51:43 -0700

    while loops in C

--- main.c
+++ main.c
@@ -3,41 +3,16 @@
 
 /*https://www.youtube.com/watch?v=KJgsSFOSQv0&t*/
 
-struct Student{
-    char name[50];
-    char major[50];
-    int age;
-    double gpa;
-
-};
-
 int main()
 {
-    printf("===Structs===\n\n");
+    printf("===While Loops===\n\n");
 
-    printf("struct - data structure where we can store groups of data types\n");
-    printf("like a list/array in python\n\n");
+    int index = 1;
+    while(index <= 5){
+        printf("%d\n", index);
+        index++; /*this is equivalent to 'index = index + 1;'*/
+    }
 
-    struct Student student1;
-    student1.age = 22;
-    student1.gpa = 3.2;
-    /*string is an array of values so you can't just do student1.name = "Jim"
-    use strcpy() function*/
-    strcpy(student1.name, "Jim");
-    strcpy(student1.major, "Business");
-
-    struct Student student2;
-    student2.age = 30;
-    student2.gpa = 4.0;
-    /*string is an array of values so you can't just do student1.name = "Jim"
-    use strcpy() function*/
-    strcpy(student2.name, "Pam");
-    strcpy(student2.major, "Business");
-
-    printf("%f\n", student1.gpa);
-    printf("%s\n\n", student1.name);
-    printf("%f\n", student2.gpa);
-    printf("%s\n\n", student2.name);
 
     return 0;
 }
