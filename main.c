356a25cf7f1e8c0620e9a84ab6a480794c881ec5
DMDay2 <damon.maldonado.1@gmail.com>
2021-07-16 04:26:19 -0700

    Structs - like a python list/array

--- main.c
+++ main.c
@@ -3,33 +3,41 @@
 
 /*https://www.youtube.com/watch?v=KJgsSFOSQv0&t*/
 
-/*
-and = &&
-or = ||
-*/
+struct Student{
+    char name[50];
+    char major[50];
+    int age;
+    double gpa;
 
-int max(int num1, int num2, int num3){
-    int result;
-    if(num1 >= num2 && num1 >= num3){ /*NOTE the ampersand symbols*/
-        result = num1;
-    } else if(num2 >= num1 && num2 >= num3){ /*note 'else if' statement*/
-        result = num2;
-    } else { /*Note 'else' statement*/
-        result = num3;
-    }
-}
+};
 
 int main()
 {
-    printf("===If/Else Statements===\n\n");
+    printf("===Structs===\n\n");
 
-    printf("%d\n\n", max(60, 70, 80));
+    printf("struct - data structure where we can store groups of data types\n");
+    printf("like a list/array in python\n\n");
 
-    if(3 < 2 || 2 > 5){ /*Note the 'or' sign (||).*/
-        printf("True");
-    } else {
-        printf("False")
-    }
+    struct Student student1;
+    student1.age = 22;
+    student1.gpa = 3.2;
+    /*string is an array of values so you can't just do student1.name = "Jim"
+    use strcpy() function*/
+    strcpy(student1.name, "Jim");
+    strcpy(student1.major, "Business");
+
+    struct Student student2;
+    student2.age = 30;
+    student2.gpa = 4.0;
+    /*string is an array of values so you can't just do student1.name = "Jim"
+    use strcpy() function*/
+    strcpy(student2.name, "Pam");
+    strcpy(student2.major, "Business");
+
+    printf("%f\n", student1.gpa);
+    printf("%s\n\n", student1.name);
+    printf("%f\n", student2.gpa);
+    printf("%s\n\n", student2.name);
 
     return 0;
 }
